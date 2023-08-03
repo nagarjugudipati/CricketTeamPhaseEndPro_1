243a8d139b16726c71f14a8f134402157e5a1aac
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-08-03 14:47:42 +0530

    CricketTeamPhaseendProj1

---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketTeamPhaseEndPro_1
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
